b5fb7f09512cda2e43e7d6ba70410d9111d4a2d0
cgeoffroy <cgeoffroy@users.noreply.github.com>
2016-03-18 13:26:50 +0100

    Fix ci test entrypoint

--- utils/ci/build_01_checks_and_unit_tests.sh
+++ utils/ci/build_01_checks_and_unit_tests.sh
@@ -8,4 +8,4 @@ docker build -t 'son-analyze-test' -f utils/docker/test.Dockerfile .
 if [ -n "${JENKINS_URL}" ]; then
     EXTRA_ENV="JENKINS_URL=${JENKINS_URL}"
 fi
-docker run -i --rm=true  --env="${EXTRA_ENV}" -v "$(pwd)/outputs:/son-analyze/outputs" 'son-analyze-test' scripts/clean.sh && scripts/all.py
+docker run -i --rm=true  --env="${EXTRA_ENV}" -v "$(pwd)/outputs:/son-analyze/outputs" 'son-analyze-test' /bin/bash -c "scripts/clean.sh && scripts/all.py"
