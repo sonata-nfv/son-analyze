6cc115d9be0cdd981b1ca3d6b32917fc359936c4
cgeoffroy <cgeoffroy@users.noreply.github.com>
2016-03-18 11:36:52 +0100

    Add env variable and volume when building on jenkins

--- utils/ci/build_01_checks_and_unit_tests.sh
+++ utils/ci/build_01_checks_and_unit_tests.sh
@@ -5,4 +5,7 @@ docker build -t 'son-analyze-deploy' -f utils/docker/deploy.Dockerfile .
 
 docker build -t 'son-analyze-test' -f utils/docker/test.Dockerfile .
 
-docker run -i --rm=true 'son-analyze-test' scripts/all.py
+if [ -n "${JENKINS_URL}" ]; then
+    EXTRA_ENV="JENKINS_URL=${JENKINS_URL}"
+fi
+docker run -i --rm=true  --env="${EXTRA_ENV}" -v "$(pwd)/outputs:/son-analyze/outputs" 'son-analyze-test' scripts/clean.sh && scripts/all.py
