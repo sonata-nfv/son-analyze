b7e4d157524f64450534408adaa88d16b7ea5b47
cgeoffroy <cgeoffroy@users.noreply.github.com>
2016-04-13 11:51:33 +0200

    Create an example file for the environment

---
+++ src/son/analyze/resources/r/examples/basic_simulation.R
@@ -0,0 +1,4 @@
+# Start a randomly generated simulation of a cpu with a small prediction
+aSimulation <- son.analyze::simulate(3, 300, 80)
+# Display the result
+son.analyze::displaySimulation(aSimulation)
