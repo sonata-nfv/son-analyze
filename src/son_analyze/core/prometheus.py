91d64401f2f76b355cf0809dcc613d63e7b6efba
cgeoffroy <cgeoffroy@users.noreply.github.com>
2016-07-04 17:26:54 +0200

    Building indexes for the prometheus data

--- src/son_analyze/core/prometheus.py
+++ src/son_analyze/core/prometheus.py
@@ -10,16 +10,25 @@ class PrometheusData:
     query
     """
 
-    raw = None  # Dict[Any, Any]
-
     def __init__(self, raw_json: str) -> None:
         """Constructor from a string containing a json structure"""
-        self.raw = json.loads(raw_json)
+        self.raw = json.loads(raw_json)  # Dict[Any, Any]
+        self._by_id = {}  # type: Dict[str, Any]
+        self._by_metric_name = {}  # type: Dict[str, Any]
+        self._build_indexes()
 
-    def some_method1(self):
-        """some_method1"""
-        pass
-
+    def _build_indexes(self) -> None:
+        """Create some indexes to provide some search shortcuts over data"""
+        if not self.is_success():
+            return
+        for elt in self.raw['data']['result']:
+            elt_id = elt['metric']['id']
+            to_update_by_id = self._by_id.get(elt_id, [])
+            self._by_id[elt_id] = to_update_by_id.append(elt)
+            elt_name = elt['metric']['__name__']
+            to_update_by_name = self._by_metric_name.get(elt_name, {})
+            to_update_by_name[elt_id] = elt
+            self._by_metric_name[elt_name] = to_update_by_name
 
     def is_success (self):
         """Test if the related data is a success or an error
--- tests/unit/son_analyze/core/prometheus_test.py
+++ tests/unit/son_analyze/core/prometheus_test.py
@@ -6,8 +6,6 @@ from son_analyze.core import prometheus
 
 def test_prometheus_data_load(basic_query_01, empty_result,
                               error_result) -> None:
-    x = prometheus.PrometheusData('[]')
-    assert x.raw == []
     x = prometheus.PrometheusData(basic_query_01)
     assert x.is_success()
     assert len(x.raw['data']['result']) == 1
@@ -15,8 +13,14 @@ def test_prometheus_data_load(basic_query_01, empty_result,
     assert x.raw['data']['result'][0]['metric']['__name__'] == 'cnt_cpu_perc'
     cnt_id = '93a02b23a465d63c3865ed421a437365c464b86e4018f229ee0f91602826b453'
     assert len(x.get_metric_values('cnt_cpu_perc', cnt_id)['values']) == 19
+    assert len(x._by_id) == 1
+    assert len(x._by_metric_name) == 1
     x = prometheus.PrometheusData(empty_result)
     assert x.is_success()
     assert x.raw['data']['result'] == []
+    assert len(x._by_id) == 0
+    assert len(x._by_metric_name) == 0
     x = prometheus.PrometheusData(error_result)
     assert not x.is_success()
+    assert len(x._by_id) == 0
+    assert len(x._by_metric_name) == 0
