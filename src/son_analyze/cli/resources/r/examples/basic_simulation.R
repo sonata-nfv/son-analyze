4601ee6baecfb91b128955e5ddfb5515f394f1a9
cgeoffroy <cgeoffroy@users.noreply.github.com>
2016-07-08 15:32:13 +0200

    Removing all R lang files

--- src/son_analyze/cli/resources/r/examples/basic_simulation.R
+++
@@ -1,4 +0,0 @@
-# Start a randomly generated simulation of a cpu with a small prediction
-aSimulation <- son.analyze::simulate(3, 300, 80)
-# Display the result
-son.analyze::displaySimulation(aSimulation)
