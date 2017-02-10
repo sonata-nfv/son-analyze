f0a3b675d923e86b3ffc1f17fd2021221a438595
Steven Van Rossem <steven.vanrossem@intec.ugent.be>
2017-02-10 20:17:11 +0100

    try to fix jenkins int14 sdk-on-analyze build error

--- utils/ci/int_01_checks_integration_tests.sh
+++ utils/ci/int_01_checks_integration_tests.sh
@@ -4,6 +4,8 @@ set -x
 
 if [ -n "${JENKINS_URL}" ]; then
     EXTRA_ENV="JENKINS_URL=${JENKINS_URL}"
+else
+    EXTRA_ENV="JENKINS_URL=empty"
 fi
 
 docker run -i --rm=true  --env="${EXTRA_ENV}" --env="INTEGRATION_TEST=1" -v "/var/run/docker.sock:/var/run/docker.sock" --workdir '/var/tmp/son.analyze' 'son-analyze:latest' /usr/bin/R -e 'devtools::test()'
