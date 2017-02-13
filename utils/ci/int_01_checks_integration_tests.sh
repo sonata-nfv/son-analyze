e250d40f5e5a4dce1b6b78d6e655ce13445bb626
cgeoffroy <cgeoffroy@users.noreply.github.com>
2017-02-13 16:38:01 +0100

    FIX add a place holder variable

--- utils/ci/int_01_checks_integration_tests.sh
+++ utils/ci/int_01_checks_integration_tests.sh
@@ -5,6 +5,8 @@ set -x
 if [ -n "${JENKINS_URL}" ]; then
     echo "Jenkins url detected: ${JENKINS_URL}"
     EXTRA_ENV="JENKINS_URL=${JENKINS_URL}"
+else
+    EXTRA_ENV="_LOCAL_RUN=1"
 fi
 
 docker run -i --rm=true  --env="${EXTRA_ENV}" --env="INTEGRATION_TEST=1" -v "/var/run/docker.sock:/var/run/docker.sock" --workdir '/var/tmp/son.analyze' 'son-analyze:latest' version
