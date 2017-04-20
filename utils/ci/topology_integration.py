fc9e3932efcb66a1f4569bce498bd0b03395d747
cgeoffroy <cgeoffroy@users.noreply.github.com>
2017-04-20 16:58:45 +0200

    Using a logger in the topology

--- utils/ci/topology_integration.py
+++ utils/ci/topology_integration.py
@@ -4,27 +4,33 @@ import sys
 import time
 import signal
 import threading
+import logging
 from emuvim.dcemulator.net import DCNetwork
 from mininet.node import RemoteController
 from emuvim.api.sonata import SonataDummyGatekeeperEndpoint
 
 
+_LOGGER = logging.getLogger(__name__)
+
+
 class SigTermCatcher:  # pylint: disable=too-few-public-methods
 
     def __init__(self, net):
+        _LOGGER.info("Setting up the signal catcher")
         self.net = net
         signal.signal(signal.SIGTERM, self.stop_containernet)
         signal.signal(signal.SIGINT, self.stop_containernet)
 
     def stop_containernet(self, signum, frame):
         msg = "Catched signal {0!s} on frame {1!s}".format(signum, frame)
-        print >>sys.stderr, msg
+        _LOGGER.warn(msg)
         self.net.stop()
         time.sleep(2)
         exit(1)
 
 
 def _in_separate_thread(net):
+    _LOGGER.info("Starting the topology")
     try:
         net.start()
     except Exception as e:
@@ -32,6 +38,7 @@ def _in_separate_thread(net):
 
 
 def setup_topology(net):
+    _LOGGER.info("Setting up the topology")
     dc = net.addDatacenter("dc")  # pylint: disable=invalid-name
     # add the SONATA dummy gatekeeper to each DC
     sdkg1 = SonataDummyGatekeeperEndpoint("0.0.0.0", 5000, deploy_sap=True)
@@ -41,6 +48,7 @@ def setup_topology(net):
 
 
 def main():
+    _LOGGER.info("Executing the integration topology")
     net = DCNetwork(controller=RemoteController,
                     monitor=True,
                     enable_learning=True)
