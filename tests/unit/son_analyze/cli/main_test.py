6a2708faed24e5e51938e9dbd18bc076adc28a51
cgeoffroy <cgeoffroy@users.noreply.github.com>
2017-08-30 15:26:37 +0200

    Skip container test when running under Jenkins

--- tests/unit/son_analyze/cli/main_test.py
+++ tests/unit/son_analyze/cli/main_test.py
@@ -71,6 +71,7 @@ def run_bg(request):
     request.addfinalizer(fin)
 
 
+@pytest.mark.skipif(os.getenv('JENKINS_URL'), reason="Failing under Jenkins")
 @pytest.mark.usefixtures("run_bg")
 def test_run(docker_cli) -> None:  # pylint: disable=redefined-outer-name
     req = None
